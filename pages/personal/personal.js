e4bdbff10575b9e0275353b4cfb98fc00c8ab168
kanji <manojgodhani00@gmail.com>
2017-12-13 19:13:37 +0530

    confirm booking display guest dynamically

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -11,6 +11,12 @@ app.controller('personalCtrl', function ($scope, $localStorage, $state, $filter)
   $scope.guestCount = 0;
 
   $scope.$storage = $localStorage;
+
+  if($localStorage.userInformation){
+    $('input').removeClass('empty');
+    $scope.personal = $localStorage.userInformation;
+  }
+
   $localStorage.bookRooms = ($localStorage.bookRooms) ? $localStorage.bookRooms : [];
 
   $scope.$storage.arrivalDate = moment($scope.$storage.arrivalDate);
@@ -43,8 +49,22 @@ app.controller('personalCtrl', function ($scope, $localStorage, $state, $filter)
         });
 
         if (form.$invalid) return;
-
+        $scope.guests = [];
+        for(var i=1; i<=$scope.guestCount;i++){
+          var guestName       = eval("$scope.personal.guestName"+i);
+          var guestEmail      = eval("$scope.personal.guestEmail"+i);
+          var guestSurname    = eval("$scope.personal.guestSurname"+i);
+          var guestTelephone  = eval("$scope.personal.guestTelephone"+i);
+          $scope.guests.push({
+            guestName : guestName,
+            guestEmail : guestEmail,
+            guestSurname : guestSurname,
+            guestTelephone : guestTelephone,
+          });
+        }
         $localStorage.userInformation = $scope.personal;
+        $localStorage.userInformation.guests = $scope.guests;
+
         $state.go('confirm');
     };
     
@@ -73,25 +93,25 @@ app.directive('addHtml', function ($compile) {
         '<div ng-messages="personalForm.guestName'+ scope.guestCount +'.$error" style="color:#9A2F21"  ng-if="personalForm.guestName'+ scope.guestCount +'.$dirty">' +
         '<div ng-message="required">Guest Name is required.</div>' +
         '</div>' +
-        '<input type="text" ng-model="personal.guestName'+ scope.guestCount +'" name="guestName'+ scope.guestCount +'" class="form-control" placeholder="Name" required>' +
+        '<input type="text" ng-model="personal.guestName'+ scope.guestCount +'" name="guestName'+ scope.guestCount +'" class="form-control" placeholder="Name" required ng-required="true">' +
         '</div>' +
         '<div class="form-group">' +
         '<div ng-messages="personalForm.guestSurname'+ scope.guestCount +'.$error" style="color:#9A2F21"  ng-if="personalForm.guestSurname'+ scope.guestCount +'.$dirty">' +
         '<div ng-message="required">Guest Surname is required.</div>' +
         '</div>' +
-        '<input type="text" ng-model="personal.guestSurname'+ scope.guestCount +'" name="guestSurname'+ scope.guestCount +'" class="form-control" placeholder="Surname" required>' +
+        '<input ng-required="true" type="text" ng-model="personal.guestSurname'+ scope.guestCount +'" name="guestSurname'+ scope.guestCount +'" class="form-control" placeholder="Surname" required>' +
         '</div>' +
         '<div class="form-group">' +
         '<div ng-messages="personalForm.guestEmail'+ scope.guestCount +'.$error" style="color:#9A2F21"  ng-if="personalForm.guestEmail'+ scope.guestCount +'.$dirty">' +
         '<div ng-message="required">E-mail address is required.</div>' +
         '</div>' +
-        '<input type="text" ng-model="personal.guestEmail'+ scope.guestCount +'" name="guestEmail'+ scope.guestCount +'" class="form-control" placeholder="E-mail address" required>' +
+        '<input ng-required="true" type="email" ng-model="personal.guestEmail'+ scope.guestCount +'" name="guestEmail'+ scope.guestCount +'" class="form-control" placeholder="E-mail address" required>' +
         '</div>' +
         '<div class="form-group">' +
         '<div ng-messages="personalForm.guestTelephone.$error'+ scope.guestCount +'" style="color:#9A2F21"  ng-if="personalForm.guestTelephone.$dirty'+ scope.guestCount +'">' +
         '<div ng-message="required">Telephone Number is required.</div>' +
         '</div>' +
-        '<input type="text" ng-model="personal.guestTelephone'+ scope.guestCount +'" name="guestTelephone'+ scope.guestCount +'" class="form-control" placeholder="Telephone number" required>' +
+        '<input ng-required="true" type="text" ng-model="personal.guestTelephone'+ scope.guestCount +'" name="guestTelephone'+ scope.guestCount +'" class="form-control" placeholder="Telephone number" required>' +
         '</div>' +
         '</div>' +
         '</div>'
